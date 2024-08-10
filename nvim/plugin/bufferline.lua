13c490732ded37ea031e2162fb74deb16a2ce828
sinh-x <sinh@sinhn.com>
2024-08-11 00:18:44 +0700

    Trial temp commit

--- nvim/plugin/bufferline.lua
+++ nvim/plugin/bufferline.lua
@@ -6,11 +6,6 @@ vim.g.did_load_bufferline_plugin = true
 require('bufferline').setup {
   options = {
     numbers = 'ordinal',
-    diagnostics = 'nvim_lsp',
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match('error') and ' ' or ' '
-      return ' ' .. icon .. count
-    end,
     show_buffer_close_icons = true,
     show_close_icon = true,
     show_tab_indicators = true,
